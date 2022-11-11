191519c18be9b6af405ed2d751d76c482fb92ff1
your name <if21b213@technikum-wien.at>
2022-11-11 17:28:22 +0100

    10.11

--- src/main/java/org/example/application/card/ElementType.java
+++ src/main/java/org/example/application/card/ElementType.java
@@ -1,14 +1,11 @@
 package org.example.application.card;
 
-import com.google.gson.annotations.SerializedName;
 
 public enum ElementType {
-    @SerializedName("FIRE")
+
     FIRE,
 
-    @SerializedName("WATER")
     WATER,
 
-    @SerializedName("NORMAL")
     NORMAL
 }
