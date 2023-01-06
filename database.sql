ec80200c4069caa2b1f165493a25fbee4ebc0126
Yin Haichuan <seahigh1@gmail.com>
2023-01-06 20:51:08 +0100

    06.01 finish

--- database.sql
+++ database.sql
@@ -1,19 +1,79 @@
-CREATE TABLE IF NOT EXISTS users (
-    username VARCHAR(255) PRIMARY KEY,
-    password VARCHAR(255) NOT NULL
+create table public.messages
+(
+    id      serial
+        primary key,
+    message varchar(255)
 );
 
-CREATE TABLE IF NOT EXISTS cards (
-    id VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    damage DECIMAL(6, 2) NOT NULL,
-    element_type VARCHAR(255)  NOT NULL,
-    card_type VARCHAR(255)  NOT NULL,
-    package_id   INT,
-    user_name    VARCHAR(255) NOT NULL
+alter table public.messages
+    owner to swe1user;
+
+create table public.users
+(
+    id            serial
+        primary key,
+    username      varchar(255)       not null,
+    status        varchar(255),
+    password      varchar(255)       not null,
+    token         varchar(255),
+    coins         integer default 20 not null,
+    total_battles integer default 0,
+    won_battles   integer default 0,
+    lost_battles  integer default 0,
+    elo           integer default 100,
+    call_name     varchar(255),
+    bio           varchar(255),
+    image         varchar(255),
+    rank          varchar(255)
 );
 
-CREATE TABLE IF NOT EXISTS packages (
-    id VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+alter table public.users
+    owner to swe1user;
+
+create table public.packages
+(
+    id    serial
+        primary key,
+    price integer default 5 not null,
+    legal integer
 );
+
+alter table public.packages
+    owner to swe1user;
+
+create table public.cards
+(
+    id           varchar(255)  not null
+        primary key,
+    name         varchar(255)  not null,
+    damage       numeric(6, 2) not null,
+    element_type varchar(255),
+    card_type    varchar(255),
+    package_id   integer
+        constraint fk_package
+            references public.packages,
+    user_id      integer
+        constraint fk_user
+            references public.users,
+    in_deck      boolean default false,
+    is_locked    boolean default false
+);
+
+alter table public.cards
+    owner to swe1user;
+
+create table public.battles
+(
+    id          serial
+        primary key,
+    player_a    varchar(255),
+    player_b    varchar(255),
+    winner      integer,
+    card_1      varchar(255),
+    card_2      varchar(255),
+    winner_card varchar(255)
+);
+
+alter table public.battles
+    owner to swe1user;
+
